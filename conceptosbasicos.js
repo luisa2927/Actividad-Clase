3d842d54e8d362e4caea1e9fa0cc746fe6995df6
luisa2927 <adrianosoriogomez1976@gmail.com>
2024-04-08 21:28:06 -0500

    actualizacion

--- conceptosbasicos.js
+++ conceptosbasicos.js
@@ -1,1 +1,82 @@
-console.log("Hola Mundo");
+//para conoctar se utiliza eslash eslash
+
+
+console.log("Hola Mundo-Psicologos-desarrolladores");
+console.log("aqui va mi mensaje");
+
+// conceptos basicos en js
+// 1. tipos de variables
+// 2. manejo del don
+// 3. condicionales
+// 4. ciclos
+
+//LAS VARIABLES EN JS
+// hay tres tipos de vaeiables en js pero nosotros solo  utilizamos dos
+// var-> ya no se usa y no la utilizamos. esta variable tambien canbia en el tiempo
+
+// LET-> Es una variable que cambia en el tiempo
+
+// CONS-> Es una variable que no puede cambiar en el tiempo
+// es igual
+
+// TIPOS DE DATOS O INFORMACION QUE PODEMOS ALMACENAR
+// 1. numericos con la declaracion LET
+let numero = 7;
+let texto = "Hola Psicologos"
+// 2. buleanos, es un dato que puede ser verdadero o falso
+let esVerdadero = true;//-> verdad
+let esFalso = false; //false -> falso
+
+// 3. arreglos o arrays (en programacion) me permiten almacenar varios datos al mismo tiempo en una variable
+let arreglamos = ["adrian","carlos","juan"];
+
+//Booleanos->lll
+
+
+
+
+
+
+
+
+
+
+
+//Objetos -> "son una representacion de elementos de la vida pero en nuestro codigo"
+
+// -> atrubutos -> caracteristicas de ese objeto
+// -> metodo -> acciones que puede realizar el objeto
+// (), {}, []
+// -> corchetes -> son para declarar acciones
+// -> () -> nos permiten ejecutar acciones
+// -> [] -> nos permiten declarar arreglos
+const celular = {
+    color:"blanco",
+    marca:"galax 7",
+    modelo: 888,
+    enviarMensajes: function (){
+    // entre estos corchetes al lado al lado de unos parentecis
+    // establezco la accion a realizar
+    console.log("mensaje enviado")
+
+    }
+
+    // El metodo -> las aciones que puedo ejecutar
+
+    
+} 
+{
+    // funciones generales -> buscar informacion
+    funcionBuscarobjetos
+    
+    {
+    function filtrarInformacion() {
+
+    }
+    }
+
+    }
+
+{
+
+}
